c1b8b2c1b8894f9a32bce5924e29ddbcd961fbac
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2019-02-27 20:20:08 -0600

    Setup Prism in all projects

---
+++ MyReadBooks/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MyReadBooks
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
